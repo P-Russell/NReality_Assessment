c34720be2ed6e54960f907e225092a482b9f0441
P-Russell <pdrussell011@gmail.com>
2020-11-14 08:45:30 +0200

    feat: initial infrastructure setup

---
+++ server/handler.py
@@ -0,0 +1,29 @@
+import json
+
+
+def feed(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def read_later(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
